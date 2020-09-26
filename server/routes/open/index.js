b2e85bbf21238c2463036e3d2f7f9562bb236b6d
e-a-w- <eawatkins.0@gmail.com>
2020-09-26 12:49:13 -0400

    api routes added and middleware started

---
+++ server/routes/open/index.js
@@ -0,0 +1,91 @@
+const router = require("express").Router(),
+  //   User = require("../../db/models/user"),
+  axios = require("axios");
+
+// Get Random Recipe
+router.get("/api/recipe/random", async (req, res) => {
+  try {
+    const { data } = await axios.get(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.SPOONACULAR}`
+    );
+    // const ingredients = data.
+    res.status(200).json(data);
+  } catch (error) {
+    if (error) {
+      console.log(error);
+      res.status(500).json({ error: error.toString() });
+    }
+  }
+});
+
+// Search Recipes
+router.post("/api/recipe/search", async (req, res) => {
+  try {
+    const { query } = req.query;
+    const { data } = await axios.get(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.SPOONACULAR}&query=${query}`
+    );
+    res.status(200).json(data);
+  } catch (error) {
+    if (error) {
+      res.status(500).json({ error: error.toString() });
+    }
+  }
+});
+
+// Get Recipe by ID
+router.get("/api/recipe/:id", async (req, res) => {
+  try {
+    const { query } = req.query;
+    const { data } = await axios.get(
+      `https://api.spoonacular.com/recipes/${req.params.id}/information?apiKey=${process.env.SPOONACULAR}&query=${query}`
+    );
+    res.status(200).json(data);
+  } catch (error) {
+    if (error) {
+      res.status(500).json({ error: error.toString() });
+    }
+  }
+});
+
+// Create New User (Register)
+router.post("/api/user", async (req, res) => {
+  const { username, email, password } = req.body;
+  let user = await User.findOne({ email });
+  if (user) res.status(409).send("that email has already been used");
+  try {
+    user = new User({
+      username,
+      email,
+      password,
+    });
+    const token = await user.generateAuthToken();
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      sameSite: "Strict",
+      secure: process.env.NODE_ENV !== "production" ? false : true,
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(401).json({ error: error.toString() });
+  }
+});
+
+// Login
+router.post("/api/user/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findByCredentials(email, password);
+    const token = await user.generateAuthToken();
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      sameSite: "Strict",
+      secure: process.env.NODE_ENV !== "production" ? false : true,
+    });
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.toString() });
+  }
+});
+
+module.exports = router;
---
+++ server/routes/secure/snacks.js
@@ -0,0 +1,84 @@
+const router = require("express").Router();
+// Snack = require("../../db/models/snack");
+
+// Create (Save) New Snack for Current User
+router.post("/api/snacks", async (req, res) => {
+  // const { ingredients } = req.body;
+  try {
+    const newSnack = new Snack({ ingredients });
+    await newSnack.save();
+    req.user.snacks.push(newSnack._id);
+    await req.user.save();
+    res.status(201).json(newSnack);
+  } catch (err) {
+    res.status(500).json({ err: err.toString() });
+  }
+});
+
+// Get All Current User's Snacks
+router.get("/api/snacks/:id", async (req, res) => {
+  try {
+    if (!req.user.snacks) {
+      res.send(404);
+    }
+    res.send(200).json(req.user.snacks);
+  } catch (error) {
+    if (error) {
+      res.status(500).json({ error: error.toString() });
+    }
+  }
+});
+
+// Get Current User's Snack By Id
+router.get("/api/snacks/:id", async (req, res) => {
+  try {
+    const snack = await Snack.findById(req.params.id);
+    if (!snack) {
+      res.send(404);
+    }
+    res.send(200).json(snack);
+  } catch (error) {
+    if (error) {
+      res.status(500).json({ error: error.toString() });
+    }
+  }
+});
+
+// Edit Current User's Snack By ID
+router.patch("/api/snacks/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["ingredients"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+  try {
+    const snack = await Snack.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json(snack);
+  } catch (err) {
+    res.status(500).json({ err: err.toString() });
+  }
+});
+
+// Delete Current User's Snack By ID
+router.delete("/api/snacks/:id", async (req, res) => {
+  try {
+    const snack = await Snack.findById(req.params.id);
+    await Author.findByIdAndUpdate(req.user._id, {
+      $pull: { snacks: { $in: [req.params.id] } },
+    });
+    snack.remove();
+    res.json(snack);
+  } catch (error) {
+    if (error) {
+      res.status(500).json({ error: error.toString() });
+    }
+  }
+});
+
+module.exports = router;
