2c760bb669efd6c50cae5d6c3a451a80050b3b7d
e-a-w- <eawatkins.0@gmail.com>
2020-09-26 16:53:08 -0400

    front end authentication setup

---
+++ client/src/context/AppContext.js
@@ -0,0 +1,33 @@
+import React, { createContext, useState, useEffect } from "react";
+import axios from "axios";
+const AppContext = createContext();
+
+const AppContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const user = sessionStorage.getItem("user");
+
+  //   useEffect(() => {
+  //     if (user && !currentUser) {
+  //       axios
+  //         .get("/api/author/me", { withCredentials: true })
+  //         .then((res) => {
+  //           setCurrentUser(res.data);
+  //         })
+  //         .catch((error) => console.log(error));
+  //     }
+  //   }, [currentUser, user, setCurrentUser]);
+
+  return (
+    <AppContext.Provider
+      value={{
+        currentUser,
+        setCurrentUser,
+        user,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export { AppContext, AppContextProvider };
